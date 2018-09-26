cd5d0316168d9171c6653d993fa98605e770def3
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-09-26 21:14:39 +0530

    fixed url path for login and logout

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -6,18 +6,26 @@ import {NavLink} from 'react-router-dom';
 
 export default class Header extends Component {
     constructor(props) {
-        super(props);
+        super(props); 
+        this.state = {
+            
+        }
     }
     
-    componentDidMount() {
-        
-    }
+    componentWillMount() {
+        var path = window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port: '');
+         
+        this.setState ({
+            redirectUri: path + '/auth/authorize/',
+            logoutUri: path + '/auth/logout/',
+        })
+         
+     }
     
     render() {
         return (
             <div> 
             <h2>Gift app</h2>
-
 
                 <NavLink to="/" 
                         exact 
@@ -42,6 +50,7 @@ export default class Header extends Component {
                 </NavLink>
 
 
+        {/*
                 <a href="https://reactapp.auth.ap-southeast-1.amazoncognito.com/login?response_type=code&client_id=24nhkmag3q6c6b43ftss4lpjft&redirect_uri=http://localhost:3000/auth/authorize/" 
                     className="button">Login with Code</a>
 
@@ -50,6 +59,18 @@ export default class Header extends Component {
                     className="button">OAuth</a>
 
             <a href="https://reactapp.auth.ap-southeast-1.amazoncognito.com/logout?client_id=24nhkmag3q6c6b43ftss4lpjft&logout_uri=http://localhost:3000/auth/logout/" 
+                    className="button">Logout</a>
+
+            */}
+            
+            <a href={"https://reactapp.auth.ap-southeast-1.amazoncognito.com/login?response_type=code&client_id=24nhkmag3q6c6b43ftss4lpjft&redirect_uri=" + this.state.redirectUri} 
+                    className="button">Login with Code</a>
+
+
+                <a href={"https://reactapp.auth.ap-southeast-1.amazoncognito.com/oauth2/authorize?response_type=token&client_id=24nhkmag3q6c6b43ftss4lpjft&redirect_uri=" +  this.state.redirectUri} 
+                    className="button">OAuth</a>
+
+            <a href={"https://reactapp.auth.ap-southeast-1.amazoncognito.com/logout?client_id=24nhkmag3q6c6b43ftss4lpjft&logout_uri=" + this.state.logoutUri}
                     className="button">Logout</a>
 
 
