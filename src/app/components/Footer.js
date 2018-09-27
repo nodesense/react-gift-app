1c368d8cacd8d5bb8b04f909fe7071234aaf517d
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-09-27 11:08:27 +0530

    test 1

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer(props) {
         return (
             <div> 
             <hr />
-            <p>Copyrights 2018, React App</p>
+            <p>Copyrights 2018, React App, fixed by Gayathri</p>
             </div>
         )
 } 
